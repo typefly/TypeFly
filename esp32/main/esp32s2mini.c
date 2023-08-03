1e09add66a59a96b2d47e21d899b9a4a59cad5c7
Guojun Chen <guojun.chen@yale.edu>
2023-08-03 17:05:12 -0400

    update file

--- esp32/main/esp32s2mini.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "driver/gpio.h"
-#include "hal/gpio_types.h"
-
-void app_main(void)
-{
-    gpio_set_direction(GPIO_NUM_33, GPIO_MODE_OUTPUT);
-    gpio_set_level(GPIO_NUM_33, 0);
-}
