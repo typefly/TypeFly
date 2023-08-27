e1866cd3f860a844be820645916362e23669c0d7
leonana69 <chengj0803@gmail.com>
2023-08-27 15:29:53 -0400

    add image test

--- tello-control.py
+++ tello-control.py
@@ -1,12 +1,26 @@
 from djitellopy import Tello
+from threading import Thread
+import cv2, time
 
 tello = Tello()
-
 tello.connect()
+
+tello.streamon()
+streamOn = True
+frame_read = tello.get_frame_read()
+
+def videoRecorder():
+    height, width, _ = frame_read.frame.shape
+
+    while streamOn:
+        # name the image file with time stamp
+        filename = f"images/{time.strftime('%Y%m%d-%H%M%S')}.jpg"
+        cv2.imwrite(filename, frame_read.frame)
+        time.sleep(1 / 30)
+
 tello.takeoff()
 
 tello.move_left(20)
-tello.rotate_counter_clockwise(90)
-tello.move_forward(20)
 
 tello.land()
+streamOn = False
